88b3660d4f4af694d3befda0a2ca5145d1e4e951
jrhoernschemeyer <58593395+jrhoernschemeyer@users.noreply.github.com>
2024-12-03 18:02:51 +0100

    Add files via upload
    1. save these to data_prep. 
    
    2. install
    conda install conda-forge::pdbfixer
    conda install conda-forge::ambertools
    
    3.
    -make 2 new folders in ./data_prep/PDB:
    a. altstructs
    b. maps
    
    4. cd to data_prep
    
    5. chmod +x ./pdbfix.sh
    
    6. ./pdbfix.sh (run the script)

--- pdbfix.sh
+++ pdbfix.sh
@@ -3,8 +3,8 @@ for file in /home/pbuser/Desktop/Jessi/data_prep/PDB/*.ent; do
     pdb=${file: -8:4}
     echo $pdb
     python3 /home/pbuser/Desktop/Jessi/data_prep/fix_pdb.py "$pdb" #pdbfixer
-    reduce -BUILD -NUC -NOFLIP -Quiet /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb-fixed.pdb > /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb-red.pdb
-    pdb2pqr --ff AMBER --with-ph 7.0 /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb-red.pdb /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb.pqr 
+    reduce -BUILD -NUC -HIS -Quiet /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb-fixed.pdb > /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb-red.pdb
+    #pdb2pqr --ff AMBER --keep-chain --assign-only --log-level WARNING --quiet /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb-red.pdb /home/pbuser/Desktop/Jessi/data_prep/PDB/$pdb.pqr 
 
 done
 
